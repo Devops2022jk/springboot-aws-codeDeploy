162ee292186ee75726dd92747c2e2bd95366ee0a
Devops2022jk <103062662+Devops2022jk@users.noreply.github.com>
2025-01-22 12:28:52 +0530

    Update TestController.java

--- src/main/java/com/example/springbootawsdeploy/TestController.java
+++ src/main/java/com/example/springbootawsdeploy/TestController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/data")
     public String getData() {
-        return "First message from AWS Ecs";
+        return "AWS CodePipeline demo from KubensLearn";
     }
 
     @GetMapping("/message")
